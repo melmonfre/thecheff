a950bd2eedea05e7ea4c4f8562a2f7128e48a396
Vitoria Marcondes <souza.marcondes@outlook.com>
2024-10-30 19:18:21 +0000

    adicao dos primeiros arquivos

---
+++ src/main/java/com/theCheff/The/Cheff/TheCheffApplication.java
@@ -0,0 +1,13 @@
+package com.theCheff.The.Cheff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCheffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCheffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theCheff/The/Cheff/TheCheffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theCheff.The.Cheff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCheffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
